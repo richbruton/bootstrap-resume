8ee9b8fc707caff2c5e5d32856c53c4a86188c0b
richbruton <richbruton@gmail.com>
2022-04-30 12:22:29 +0000

    github api set up

---
+++ assets/js/github-information.js
@@ -0,0 +1,14 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub user name</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+        <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`
+    );
+}
